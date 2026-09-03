ac2bacb7c5aa947bac972eec3a8f29ade9626327
VISHNU PRAKASH P <v3vishnuprakash.8696@gmail.com>
2026-09-03 09:59:46 +0530

    Update starter.sql

--- starter.sql
+++ starter.sql
@@ -1,6 +1,13 @@
-CREATE TABLE Department
+create table department45
 (
-DepartmentID INT(5) PRIMARY KEY,
-DepartmentName VARCHAR(20),
+departmentID INT(5)PRIMARY KEY,
+departmentname VARCHAR(20),
 HOD VARCHAR(20)
 );
+desc department45;
+INSERT INTO department45(departmentID,departmentname,HOD)
+VALUES
+(101,'cs','kumar'),
+(102,'it','ravi');
+select*from department45;
+drop table department;
